0f35c25e996423c77660194a42c5773e8a009610
Falk Hoppe <falkhoppe81@gmail.com>
2014-10-21 08:02:31 +0200

    finished first design study goals

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,10 @@ var gulp = require('gulp'),
     util = require('gulp-util'),
     watch = require('gulp-watch'),
     http = require('http'),
+    livereload = require('gulp-livereload'),
     stylus = require('gulp-stylus'),
     nib = require('nib'),
+    jeet = require('jeet'),
     rupture = require('rupture'),
     ecstatic = require('ecstatic');
 
@@ -11,13 +13,15 @@ var stylusRoot = __dirname + '/src/stylus';
 
 gulp.task('app.stylus', function () {
   gulp.src(stylusRoot + '/app.styl')
-    .pipe(stylus({use: [nib(), rupture()]}))
+    .pipe(stylus({use: [nib(), rupture(), jeet()]}))
     .pipe(gulp.dest('./build/css'));
 });
 
 
 gulp.task('watch', function() {
+  livereload.listen();
   gulp.watch(stylusRoot + '/**/*.styl', ['app.stylus'])
+  .on('change', livereload.changed);
 });
 
 
